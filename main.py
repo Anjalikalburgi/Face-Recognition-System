b41ce48a3637f3207f1cd5c89dfdb6a53d0226c4
9535555301 <dinakaryaramati95355@gmail.com>
2023-11-29 11:09:46 +0530

    added comments

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
+
 from tkinter import *
 from tkinter import ttk
-from PIL import Image,ImageTk
+from PIL import Image, ImageTk
 
-class Face_Recognition_System:
+class FaceRecognitionSystem:
 
-    def __init__(self,root):
+    def __init__(self, root):
+        # Initialize the root window
         self.root = root
-        self.root.geometry("1590x790+0+0")  #heightxwidth+x-axis+y=axis
-        self.root.title("Face Recognition System")
-
+        self.root.geometry("1590x790+0+0")  # Set the window size and position
+        self.root.title("Face Recognition System")  # Set the window title
 
 if __name__ == "__main__":
+    # Create the main Tkinter window
     root = Tk()
-    obj = Face_Recognition_System(root)
-    root.mainloop()
+
+    # Create an instance of the FaceRecognitionSystem class
         
         
