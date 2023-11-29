470f42d924b4665a216ee66c1cce13e42f7cfefe
ganesh9357 <ganeshmirashi62@gmail.com>
2023-11-29 12:06:01 +0530

    print message

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ class FaceRecognitionSystem:
         self.root.geometry("1590x790+0+0")  #heightxwidth+x-axis+y=axis
         self.root.title("Face Recognition System")
 # Add a comment to demonstrate Git changes
+
         print("This is a new line added for Git demonstration.")
+        # Add another line for Git demonstration
+        print("This is another line added for Git demonstration.")
 
 if __name__ == "__main__":
     # Create the main Tkinter window
