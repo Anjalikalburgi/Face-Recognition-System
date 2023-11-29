80c3dbb91ae645b918a92da7402ae80bcb59b7b2
RahulGaniger <rahulganiger1709@gmail.com>
2023-11-29 09:26:34 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from tkinter import ttk
+from PIL import Image,ImageTk
+
+class Face_Recognition_System:
+
+    def __init__(self,root):
+        self.root = root
+        self.root.geometry("1590x790+0+0")  #heightxwidth+x-axis+y=axis
+        self.root.title("Face Recognition System")
+
+
+if __name__ == "__main__":
+    root = Tk()
+    obj = Face_Recognition_System(root)
+    root.mainloop()
+        
+        
